6dff7835a2f148957f4f59defac278979f57f79c
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-03-28 18:47:12 +0100

    Added Routing and refactored folder structure

--- src/component/cards/CountryDetails.tsx
+++ src/component/cards/CountryDetails.tsx
@@ -2,7 +2,6 @@ import { useParams } from "react-router-dom";
 import data from "../../data/data.json";
 import "./countryDetails.css";
 
-const addCommasToNumber = (num: number): string => num.toLocaleString();
 
 function CountryDetails() {
   const { countryCode } = useParams();
