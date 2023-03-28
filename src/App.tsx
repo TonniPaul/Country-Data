18b8bb42475662581e4286fe7288fa69af80b8c2
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-03-28 01:26:55 +0100

    displayed countries data from data.json

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import CountryDataCard from "./component/cards/CountryDataCard";
 import Input from "./component/main/Input";
 import Navbar from "./component/navbar/Navbar";
+import data from "./data/data.json";
 
 function App() {
   return (
@@ -9,7 +10,20 @@ function App() {
       <Navbar />
       <main className="app_main_container">
         <Input />
-        <CountryDataCard />
+        <div className="app_data">
+          {data.map((data, index) => {
+            return (
+              <CountryDataCard
+                key={index}
+                flag={data.flag}
+                country={data.name}
+                population={data.population}
+                region={data.region}
+                capital={data.capital}
+              />
+            );
+          })}
+        </div>
       </main>
     </div>
   );
--- src/component/cards/CountryDataCard.tsx
+++ src/component/cards/CountryDataCard.tsx
@@ -5,32 +5,39 @@ interface Props {
   country: string;
   population: number;
   region: string;
-  capital: string;
+  capital?: string;
+}
+function addCommasToNumber(num: number): string {
+  return num.toLocaleString();
 }
 
-const CountryDataCard = () => {
+const CountryDataCard = ({
+  flag,
+  country,
+  population,
+  region,
+  capital,
+}: Props) => {
+  const formattedValue = addCommasToNumber(population);
   return (
     <div className="card_main_container">
       <div className="country_flag_container">
-        <img
-          src="https://flagcdn.com/as.svg"
-          alt="country"
-          className="country_flag"
-        />
-        <div className="country_data_container">
-          <h3 className="country_name">Germany</h3>
+        <img src={flag} alt={`${country} flag`} className="country_flag" />
+      </div>
+      <div className="country_data_container">
+        <h3 className="country_name"> {country} </h3>
 
-          <div className="country_data_sub_container">
-            <p className="data_text">
-              <span className="bold_text">Population : </span>81,000,000
-            </p>
-            <p className="data_text">
-              <span className="bold_text">Region : </span> Europe
-            </p>
-            <p className="data_text">
-              <span className="bold_text">Capital : </span>Balablu
-            </p>
-          </div>
+        <div className="country_data_sub_container">
+          <p className="data_text">
+            <span className="bold_text">Population : </span>
+            {formattedValue}
+          </p>
+          <p className="data_text">
+            <span className="bold_text">Region : </span> {region}
+          </p>
+          <p className="data_text">
+            <span className="bold_text">Capital : </span> {capital}
+          </p>
         </div>
       </div>
     </div>
